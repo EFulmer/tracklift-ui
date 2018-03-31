ff8dec753d352baac6902ec5140bbab96fedef78
Eric Fulmer <eric.r.fulmer@gmail.com>
2018-03-31 16:32:31 -0400

    create (nonfunctional) navbar

--- src/App.js
+++ src/App.js
@@ -1,21 +1,63 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { Navbar, Nav, NavItem } from 'react-bootstrap';
 import './App.css';
+
+class NavbarItems extends Component {
+  render() {
+    if (this.props.loggedIn) {
+      return (
+        <Nav>
+          <NavItem eventKey={1} href="#">
+            Add Workout
+          </NavItem>
+          <NavItem eventKey={2} href="#">
+            Workout Log
+          </NavItem>
+        </Nav>
+      );
+    }
+    else {
+      return (
+        <Nav>
+          <NavItem eventKey={1} href="#">
+            Log In
+          </NavItem>
+          <NavItem eventKey={2} href="#">
+            Sign Up
+          </NavItem>
+        </Nav>
+      );
+    }
+  }
+}
+
+class MyNavbar extends Component {
+  render() {
+    return (
+      <Navbar>
+        <Navbar.Header>
+          <Navbar.Brand>
+            <a href="#home">Tracklift</a>
+          </Navbar.Brand>
+        </Navbar.Header>
+        <NavbarItems loggedIn={this.props.loggedIn} />
+      </Navbar>
+    );
+  }
+}
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
+        <MyNavbar />
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          Hello React!
         </p>
       </div>
     );
   }
 }
+
 
 export default App;
