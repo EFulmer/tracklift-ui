1e678ee3b2563a02de26529ed2f9684c99cb70d9
Eric Fulmer <eric.r.fulmer@gmail.com>
2018-04-02 19:54:06 -0400

    add (nonfunctional) login form

--- src/App.js
+++ src/App.js
@@ -1,9 +1,35 @@
 import React, { Component } from 'react';
 import { Navbar, Nav, NavItem } from 'react-bootstrap';
+import { LocalForm, Control, combineForms, Errors } from 'react-redux-form';
+import { createStore } from 'redux';
 import './App.css';
+
+const required = (val) => val && val.length;
+const isEmail = (val) => {
+  // Regex from https://stackoverflow.com/a/1373724/1893155
+  const re = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/i;
+  return re.test(String(val));
+}
+
+const initialLogin = {
+  email: '',
+  password: '',
+}
+
+const initialRegister = {
+  email: '',
+  password: '',
+  confirmPassword: '',
+}
+
+const store = createStore(combineForms({
+  login: initialLogin,
+  register: initialRegister,
+}));
 
 class NavbarItems extends Component {
   render() {
+    // TODO  gonna need React Router here sometime!
     if (this.props.loggedIn) {
       return (
         <Nav>
@@ -46,6 +72,64 @@ class MyNavbar extends Component {
   }
 }
 
+class Login extends Component {
+  handleSubmit(v) {
+    // TODO  hash the password and store that in the form!
+    alert(JSON.stringify(v));
+  }
+  render() {
+    // TODO  line the elements of this form up in a nice grid
+    // TODO  assert password validity guides!
+    // TODO  assert that email is valid!
+    return (
+      <LocalForm model="login" onSubmit={v => this.handleSubmit(v)}>
+        <div className="field">
+          <label>Email:</label>
+          <Control.text 
+            type="email"
+            model="login.email"
+            validators={{
+              required,
+              isEmail,
+            }}
+          />
+          <Errors
+            className="errors"
+            model="login.email"
+            show="touched"
+            messages={{
+              required: 'Email is required'
+            }}
+          />
+        </div>
+        <div className="field">
+          <label>Password:</label>
+          <Control.text
+            model="login.password"
+            validators={{
+              required
+            }}
+          />
+          <Errors
+            className="errors"
+            model="login.password"
+            show="touched"
+            messages={{
+              required: 'Password is required!'
+            }}
+          />
+        </div>
+        <button type="submit">
+          Submit
+        </button>
+        <button type="submit">
+          Forgot your password?
+        </button>
+      </LocalForm>
+    );
+  }
+}
+
 class App extends Component {
   render() {
     return (
@@ -54,6 +138,7 @@ class App extends Component {
         <p className="App-intro">
           Hello React!
         </p>
+        <Login />
       </div>
     );
   }
